ab3f607a6c24cbe40be61f2cea74caeaa996a5e5
Daniel Pytel <danielrpytel@gmail.com>
2023-06-15 20:18:27 -0500

    structure

--- Model/categoryModel.js
+++ Model/categoryModel.js
@@ -3,110 +3,110 @@ var sql = require("../db");
 
 //Category object constructor
 var Category = function (category) {
-  this.category_name = category.category_name;
+	this.category_name = category.category_name;
 };
 
 Category.createCategory = function (newCat, result) {
-  sql.query("INSERT INTO category set ?", newCat, function (err, res) {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-    } else {
-      result(null, res.insertId);
-    }
-  });
+	sql.query("INSERT INTO category set ?", newCat, function (err, res) {
+		if (err) {
+			console.log("error: ", err);
+			result(err, null);
+		} else {
+			result(null, res.insertId);
+		}
+	});
 };
 
 Category.getCategoryById = function (categoryId, result) {
-  sql.query(
-    "SELECT category_id, category_name from category WHERE category_id = ? ",
-    categoryId,
-    function (err, res) {
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-      } else {
-        result(null, res);
-      }
-    }
-  );
+	sql.query(
+		"SELECT category_id, category_name from category WHERE category_id = ? ",
+		categoryId,
+		function (err, res) {
+			if (err) {
+				console.log("error: ", err);
+				result(err, null);
+			} else {
+				result(null, res);
+			}
+		}
+	);
 };
 
 Category.getOneCategory = function (id, callback) {
-  let sqlStr = `SELECT * FROM category WHERE category_id = ?`;
+	let sqlStr = `SELECT * FROM category WHERE category_id = ?`;
 
-  sql.query(sqlStr, id, function (err, result) {
-    if (err) throw err;
-    //console.log(result);
+	sql.query(sqlStr, id, function (err, result) {
+		if (err) throw err;
+		//console.log(result);
 
-    if (result.length) {
-      return result[0];
-    } else {
-      return null;
-    }
-  });
+		if (result.length) {
+			return result[0];
+		} else {
+			return null;
+		}
+	});
 };
 
 Category.getOneCategory = function (id, callback) {
-  let sqlStr = `SELECT * FROM category WHERE category_id = ?`;
+	let sqlStr = `SELECT * FROM category WHERE category_id = ?`;
 
-  sql.query(sqlStr, id, function (err, result) {
-    if (err) throw err;
-    //console.log(result);
+	sql.query(sqlStr, id, function (err, result) {
+		if (err) throw err;
+		//console.log(result);
 
-    if (result.length) {
-      callback(result[0]);
-    } else {
-      callback(null);
-    }
-  });
+		if (result.length) {
+			callback(result[0]);
+		} else {
+			callback(null);
+		}
+	});
 
-  if (result.length) {
-    callback(result[0]);
-  } else {
-    callback(null);
-  }
+	if (result.length) {
+		callback(result[0]);
+	} else {
+		callback(null);
+	}
 };
 
 Category.getAllCategories = function (result) {
-  sql.query("Select * from category", function (err, res) {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-    } else {
-      result(null, res);
-    }
-  });
+	sql.query("Select * from category", function (err, res) {
+		if (err) {
+			console.log("error: ", err);
+			result(null, err);
+		} else {
+			result(null, res);
+		}
+	});
 };
 
 Category.updateById = function (id, task, result) {
-  sql.query(
-    "UPDATE tasks SET task = ? WHERE id = ?",
-    [task.task, id],
-    function (err, res) {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-      } else {
-        result(null, res);
-      }
-    }
-  );
+	sql.query(
+		"UPDATE tasks SET task = ? WHERE id = ?",
+		[task.task, id],
+		function (err, res) {
+			if (err) {
+				console.log("error: ", err);
+				result(null, err);
+			} else {
+				result(null, res);
+			}
+		}
+	);
 };
 
 Category.remove = function (id, result) {
-  sql.query(
-    "DELETE FROM category WHERE category_id = ?",
-    [id],
-    function (err, res) {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-      } else {
-        result(null, res);
-      }
-    }
-  );
+	sql.query(
+		"DELETE FROM category WHERE category_id = ?",
+		[id],
+		function (err, res) {
+			if (err) {
+				console.log("error: ", err);
+				result(null, err);
+			} else {
+				result(null, res);
+			}
+		}
+	);
 };
 
 module.exports = Category;
--- Model/locationModel.js
+++ Model/locationModel.js
@@ -1,78 +1,78 @@
 "use strict";
-var sql = require("./db");
+var sql = require("../db");
 
 //Location object constructor
 var Location = function (location) {
-  this.location_name = location.location_name;
-  this.floor = location.floor;
+	this.location_name = location.location_name;
+	this.floor = location.floor;
 };
 
 // Query the database to insert new object into location table
 Location.createLocation = function (newLocation, result) {
-  sql.query("INSERT INTO location set ?", newLocation, function (err, res) {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-    } else {
-      //console.log("res in locationModel.js: ", res);
-      //console.log("locationModel.js res.insertID: ", res.insertId);
-      // insertID is the id of the new object created
-      result(null, res.insertId);
-    }
-  });
+	sql.query("INSERT INTO location set ?", newLocation, function (err, res) {
+		if (err) {
+			console.log("error: ", err);
+			result(err, null);
+		} else {
+			//console.log("res in locationModel.js: ", res);
+			//console.log("locationModel.js res.insertID: ", res.insertId);
+			// insertID is the id of the new object created
+			result(null, res.insertId);
+		}
+	});
 };
 
 // query database to get all items from location table
 // order the locations by name then floor numbers
 Location.getAllLocations = function (result) {
-  sql.query(
-    "Select * from location ORDER BY location_name ASC, floor",
-    function (err, res) {
-      //check for errors, if none then send data back
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-      } else {
-        //console.log('location : ', res);
-        result(null, res);
-      }
-    }
-  );
+	sql.query(
+		"Select * from location ORDER BY location_name ASC, floor",
+		function (err, res) {
+			//check for errors, if none then send data back
+			if (err) {
+				console.log("error: ", err);
+				result(null, err);
+			} else {
+				//console.log('location : ', res);
+				result(null, res);
+			}
+		}
+	);
 };
 
 // Query database to get specific location information by location id
 Location.getLocationById = function (id, result) {
-
-  sql.query(
-    "SELECT * FROM location WHERE location_id = ? ",
-     id,
-     function (err, res) {
-       //check for errors, if none, send data back
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-      } else {
-        //console.log('location : ', res);
-        result(null, res);
-      }
-  });
+	sql.query(
+		"SELECT * FROM location WHERE location_id = ? ",
+		id,
+		function (err, res) {
+			//check for errors, if none, send data back
+			if (err) {
+				console.log("error: ", err);
+				result(null, err);
+			} else {
+				//console.log('location : ', res);
+				result(null, res);
+			}
+		}
+	);
 };
 
 // Query database to delete specific location item by the id
 Location.remove = function (id, result) {
-  sql.query(
-    "DELETE FROM location WHERE location_id = ?",
-    [id],
-    function (err, res) {
-      //check for errors, if none then send data back of item deleted
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-      } else {
-        result(null, res);
-      }
-    }
-  );
+	sql.query(
+		"DELETE FROM location WHERE location_id = ?",
+		[id],
+		function (err, res) {
+			//check for errors, if none then send data back of item deleted
+			if (err) {
+				console.log("error: ", err);
+				result(null, err);
+			} else {
+				result(null, res);
+			}
+		}
+	);
 };
 
 module.exports = Location;
